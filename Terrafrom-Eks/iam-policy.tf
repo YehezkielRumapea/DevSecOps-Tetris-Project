12d8f89385307049370285526192bc9a7e02a768
yehezkiel <rumapeayehezkiel@gmail.com>
2026-04-14 15:15:35 +0700

    fixing tf7

--- Terrafrom-Eks/iam-policy.tf
+++ Terrafrom-Eks/iam-policy.tf
@@ -5,15 +5,15 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.EKSWorkerNode_role.name
+  role       = aws_iam_role.NodeGroupRole_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.EKSWorkerNode_role.name
+  role       = aws_iam_role.NodeGroupRole.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.EKSWorkerNode_role.name
+  role       = aws_iam_role.NodeGroupRole.name
 }
